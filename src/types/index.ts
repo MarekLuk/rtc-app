6e1c810440ffc7c964d0b57156afd9ecb570e79e
marek.lukaszcz@gmail.com <marek.lukaszcz@gmail.com>
2025-06-29 01:10:23 +0200

    added types

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,58 @@ export interface AppConfig {
     };
 }
 
+export interface ApiStateResponse {
+    odds: string;
+}
+
+export interface ApiMappingsResponse {
+    mappings: string;
+}
+
+export interface ParsedMapping {
+    [key: string]: string;
+}
+
+export interface ScoreData {
+    type: string;
+    home: string;
+    away: string;
+}
+
+export interface CompetitorData {
+    type: 'HOME' | 'AWAY';
+    name: string;
+}
+
+export interface SportEvent {
+    id: string;
+    status: string;
+    scores: { [key: string]: ScoreData };
+    startTime: string;
+    sport: string;
+    competitors: {
+        HOME: CompetitorData;
+        AWAY: CompetitorData;
+    };
+    competition: string;
+}
+
+export interface ApplicationState {
+    [eventId: string]: SportEvent;
+}
+
+export interface RawSportEventData {
+    id: string;
+    sportId: string;
+    competitionId: string;
+    startTime: string;
+    homeCompetitorId: string;
+    awayCompetitorId: string;
+    statusId: string;
+    scores: string;
+}
+
+export interface SyncedApiData {
+    state: ApiStateResponse;
+    mappings: ApiMappingsResponse;
+}
